300f1a3758fa0f6a28855bbbf5d2cc5f686efd25
ProvLeon <providence.leonard@gmail.com>
2023-04-03 18:13:43 +0100

    Updated N-queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,24 +1,25 @@
 import sys
+"""Solves the N-queens puzzle"""
 
 def nqueens(N):
-    # Check if N is an integer
+    """ Check if N is an integer"""
     try:
         N = int(N)
     except ValueError:
         print("N must be a number")
         sys.exit(1)
         
-    # Check if N is at least 4
+    """ Check if N is at least 4 """
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
         
-    # Initialize the board
+    """ Initialize the board """
     board = [-1] * N
-    
-    # Recursive function to find all solutions
+
+    """Recursive function to find all solutions """
     def place_queen(row, column):
-        # Check if the queen can be placed in this column
+        """ Check if the queen can be placed in this column """
         for i in range(row):
             if board[i] == column or \
                board[i] - i == column - row or \
@@ -37,7 +38,7 @@ def nqueens(N):
                 
     solve(0)
 
-# Check if the correct number of arguments is provided
+""" Check if the correct number of arguments is provided """
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
     sys.exit(1)
