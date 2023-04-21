5f7f372394fa1e574d54f85051635ac8948c0cc7
ProvLeon <providence.leonard@gmail.com>
2023-04-21 18:23:58 +0100

    new file:   0x0C-python-almost_a_circle/models/__pycache__/base.cpython-311.pyc 	new file:   0x0C-python-almost_a_circle/models/rectangle.py updated

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,73 @@
+from models.base import Base
+
+class Rectangle(Base):
+    """A class that represents a rectangle.
+
+    Args:
+        Base: an argument passed
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new rectangle instance.
+
+        Args:
+            width: width of the rectangle
+            height: height of th rectangle
+            x: optional integer passed. Defaults to 0.
+            y: optional integer passed. Defaults to 0.
+            id: id of the rectangle. Defaults to None.
+        """
+        super().__init__(id)
+        """only that
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """with object
+
+        Returns: width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Returns the height of the rectangle
+
+        Returns:
+            : height of the rectangle
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the heght of the rectangle"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """self for x
+
+        Returns:
+            : returns x
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
