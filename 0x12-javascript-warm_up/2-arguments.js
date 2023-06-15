361c9defd656d8e1bf5918c1483d94b672f95f22
ProvLeon <providence.leonard@gmail.com>
2023-06-15 08:54:34 +0100

    Updated

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
-let arg = process.argv.length
+const arg = process.argv.length;
 if (arg === 2) {
-    console.log("No argument");
+  console.log('No argument');
 } else if (arg === 3) {
-    console.log("Argument found");
-}
-else {
-    console.log("Arguments found");
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
