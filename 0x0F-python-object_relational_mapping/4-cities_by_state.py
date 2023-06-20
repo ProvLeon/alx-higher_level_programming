c0cd897ab8ad291de7e43ab3d8d70c06f1059658
ProvLeon <providence.leonard@gmail.com>
2023-06-20 12:03:29 +0100

    script to list all cities in a database

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This List all cities from any specified database
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name\
+    FROM cities JOIN states ON cities.state_id = states.id;")
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
