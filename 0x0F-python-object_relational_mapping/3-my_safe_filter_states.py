ad4c467f649a0965f81cdc0054b216b887c4e19a
ProvLeon <providence.leonard@gmail.com>
2023-06-20 11:22:58 +0100

    added a docstring to file

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
-Lists all values in the states tables of a database where name
+Lists all values in the states tables of a specified database where name
 matches the argument in a safe way
 """
 import sys
 import MySQLdb
+
 
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
