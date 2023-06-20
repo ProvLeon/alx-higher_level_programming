904d6bb0bda9fa8e9e5050bdb6f5b000ed81326e
ProvLeon <providence.leonard@gmail.com>
2023-06-20 10:50:09 +0100

    added extra line after module import

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,15 +5,15 @@ Lists all states with a name starting with N
 import sys
 import MySQLdb
 
+
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states \
-    WHERE CONVERT(`name` USING Latin1) \
-    COLLATE Latin1_General_CS \
-    LIKE 'N%';")
+    cursor.execute("SELECT * FROM states\
+        WHERE name LIKE 'N%'\
+        ORDER BY id ASC")
     states = cursor.fetchall()
 
     for state in states:
