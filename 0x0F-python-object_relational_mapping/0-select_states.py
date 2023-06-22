d72b1997e4097707db3412612e406552c044ff2e
ProvLeon <providence.leonard@gmail.com>
2023-06-22 23:36:59 +0100

    update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,13 +10,6 @@ import MySQLdb
 # run as the main program. If it is, then the code inside the block will be executed. This is a common
 # Python idiom used to prevent code from being executed when a module is imported as a library.
 if __name__ == '__main__':
-    # `db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)` is
-    # establishing a connection to a MySQL database using the credentials provided as command line
-    # arguments. `sys.argv[1]` is the first argument, which is the username, `sys.argv[2]` is the
-    # second argument, which is the password, and `sys.argv[3]` is the third argument, which is the
-    # name of the database. The `port` parameter specifies the port number to use for the connection,
-    # which is 3306 for MySQL. The `MySQLdb.connect()` method returns a connection object that can be
-    # used to interact with the database.
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
 
