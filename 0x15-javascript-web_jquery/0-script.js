b3d7c93b3890e2a7af87cfcf293a70a589e71b22
ProvLeon <providence.leonard@gmail.com>
2024-02-28 17:50:09 +0000

    done with task one

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,7 @@
+// 0-script.js
+// a query to update the text color of the HTML tag HEADER to red (#FF0000)
+// using the DOM API to get the header element
+const headerElement = document.querySelector('header')
+
+// update the text color of the HTML tag HEADER to red (#FF0000)
+headerElement.style.color = '#FF0000';
