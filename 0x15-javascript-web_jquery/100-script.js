2eece68c75b193eda4cc345321c168a0855c56cd
ProvLeon <providence.leonard@gmail.com>
2024-02-28 19:10:00 +0000

    new file:   0x15-javascript-web_jquery/100-main.html new file:   0x15-javascript-web_jquery/100-script.js

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,8 @@
+/* global $ */
+/**
+ * This script updates the text color of the <header> element to red (#FF0000).
+ * It uses document.querySelector to select the HTML tag and does not use the jQuery API.
+ */
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('header').style.color = '#FF0000';
+});
