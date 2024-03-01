0f831adf1dd9d7e7d77859e4749580b41649e684
ProvLeon <providence.leonard@gmail.com>
2024-03-01 05:17:28 +0000

    updated task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,18 @@
 #!/usr/bin/node
+// const request = require('request');
+// const url = 'http://swapi.co/api/films/' + process.argv[2];
+// request(url, function (error, response, body) {
+//   console.log(error || JSON.parse(body).title);
+// });
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
+const movieId = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+
 request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+  if (!error && response.statusCode == 200) {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
+  } else {
+    console.error('Error:', error);
+  }
 });
