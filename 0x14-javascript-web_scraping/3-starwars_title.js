5e4e1069be25b579099bbb4bd1f0aa13ee8f8346
ProvLeon <providence.leonard@gmail.com>
2024-03-01 05:19:13 +0000

    added strict type check for the response status code.

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -9,7 +9,7 @@ const movieId = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
 request(url, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
+  if (!error && response.statusCode === 200) {
     const movie = JSON.parse(body);
     console.log(movie.title);
   } else {
